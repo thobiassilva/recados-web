f1669f9db0fce8f23d0547713d7bf1067970a704
Thobias <thobiaspsilva@yahoo.com.br>
2021-11-13 02:00:51 -0300

    Ajusta a porta do servidor

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import express from "express";
 
 let app = express();
+app.use(express.json());
 app.use(express.static(__dirname + "/../public"));
 
-app.listen(8082, () => console.log("Servidor iniciou"));
+app.listen(process.env.PORT || 8081, () => console.log("Server ON"));
