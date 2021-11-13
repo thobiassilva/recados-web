8e98b2fd9d7dc9cac317e3a8af13996ffc57b585
Thobias <thobiaspsilva@yahoo.com.br>
2021-11-13 01:53:54 -0300

    Deleta arquivos nao utilizados

--- public/api.js
+++
@@ -1,6 +0,0 @@
-const apiGithub = axios.create({
-    baseURL: "https://api.github.com/users/thobiassilva"
-});
-
-apiGithub.get("/").then((result) => console.log(result.data));
-apiGithub.get("https://api.github.com/users/thobiassilva").then((result) => console.log(result.data));
